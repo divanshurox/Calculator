5573e3b9534d2211cc7a77d393b72662c5eeb3d6
Divanshu Agarwal <divanshuagarwal2001@gmail.com>
2022-05-15 14:41:26 +0530

    init3

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class CalculatorTest {
+	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+		CalcModel calcModel = new CalcModel();
+        try {
+        	calcModel.addTwoNumbers(1, 2);
+            assertEquals(calcModel.getCalcAddValue(),3);
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }
+    }
+}
